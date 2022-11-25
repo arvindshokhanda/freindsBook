91377bc1e93a98f6569096244cb5e9bae7732d28
arvindshokhanda <arvindshokhanda21@gmail.com>
2022-11-25 15:16:55 +0530

    create controllers

---
+++ controllers/home_controller.js
@@ -0,0 +1,7 @@
+module.exports.home = function(req, res){
+    res.end('<h1>My express server is running and up</h1>');
+}
+module.exports.practice = function(req, res){
+    
+    res.end('<h1>This  is the practice page</h1>');
+}
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-
+app.use('/', require('./routes'));
 app.listen(port, function(err){
     if(err){
         console.log(`Error in running in the server: ${err} `);
