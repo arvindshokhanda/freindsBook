59e749da9d4e0455bdf5536f9f8d455909e1df4d
arvindshokhanda <arvindshokhanda21@gmail.com>
2022-11-25 17:47:48 +0530

    set up files access for layout and pages

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,8 @@
 module.exports.home = function(req, res){
-    res.end('<h1>My express server is running and up</h1>');
+    // res.end('<h1>My express server is running and up</h1>');
+    return res.render('home',{
+        title:"Home"
+    });
 }
 module.exports.practice = function(req, res){
     
--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+//use ejs-layout
+const expressLayout = require('express-ejs-layouts');
+app.use(expressLayout);
 
+//using assets
+app.use(express.static('./assets'))
+
+//extract styles and files from sub pages to layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true)
+
+//use express router
 app.use('/', require('./routes'));
+//set up the view enjine
+app.set('view engine', 'ejs');
+app.set('views','./views');
+
 app.listen(port, function(err){
     if(err){
         console.log(`Error in running in the server: ${err} `);
