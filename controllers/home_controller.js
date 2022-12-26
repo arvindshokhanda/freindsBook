cdd07001bdc56893b67ac61975a2edf8b38a3222
arvindshokhanda <arvindshokhanda21@gmail.com>
2022-12-26 10:04:12 +0530

    converted controllers to async await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,27 +1,30 @@
 const Post = require('../models/post');
 const User  = require('../models/user');
 
-module.exports.home = function(req, res){
+module.exports.home = async function(req, res){
 
-    //populate the each user
-   Post.find({})
-   .populate('user')
-   .populate({
-        path: 'comments',
-        populate: {
-            path: "user"
-        }
-   })
-   .exec(function(err, post){
-    User.find({}, function(err, user){
-        return res.render('home',{
-            title:"friendsBook | Home",
-            post: post,
-            all_users: user
-        });
-    })
-    
-   })
+    try{
+                //populate the each user
+        let post = await Post.find({})
+        .populate('user')
+        .populate({
+                path: 'comments',
+                populate: {
+                    path: "user"
+                }
+        })
+        let user = await User.find({});
+            return res.render('home',{
+                title:"friendsBook | Home",
+                post: post,
+                all_users: user
+            });
+    }catch(err){
+        console.log("error", err);
+        return;
+    }
+   
+
 }
 
 
